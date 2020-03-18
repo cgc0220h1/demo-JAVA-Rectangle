dbdb9917580fa5bd6214c9d7a00befa4d790e0bd
vanduc2514 <vanduc2514@gmail.com>
2020-03-18 23:33:16 +0700

    First commit

---
+++ src/Rectangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        float width; {
+            System.out.println("Nhập vào chiều rộng: ");
+            width = scanner.nextFloat();
+        }
+
+        float height; {
+            System.out.println("Nhập vào chiều dài: ");
+            height = scanner.nextFloat();
+        }
+
+        float area = width * height; {
+            System.out.println("Diện tích hình chữ nhật là: " + area);
+        }
+    }
+}
